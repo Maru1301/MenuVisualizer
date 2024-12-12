cae34994266f544c695bc860a1f07ccf3671cbcd
maru_lin <maru_lin@17life.com>
2024-12-12 10:49:30 +0800

    Add: MenuVisualizerTest

---
+++ MenuVisualizerTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using MenuVisualizer;
+using MenuVisualizer.Model;
+
+namespace MenuVisualizerTest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var menu = new Menu()
+            {
+                Name = "Test",
+                Descriptions = 
+                [
+                    "Description"
+                ],
+                Options = 
+                [
+                    new SubMenuOption
+                    {
+                        Name = "Sub",
+                        SubMenu =  new Menu()
+                        {
+                            Name = "Test",
+                            Descriptions =
+                            [
+                                "Description"
+                            ],
+                        }
+                    }
+                ]
+            };
+
+            var manager = new ConsoleMenuManager();
+            manager.Construct(menu);
+
+            await manager.ShowAsync();
+        }
+    }
+}
